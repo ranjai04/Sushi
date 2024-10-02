93bd52b64c124926e6ea4c315c1191cfeab6815a
Ranjai G <115529100+ranjai04@users.noreply.github.com>
2024-10-02 23:45:41 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//
+
+
+// init AOS animation
+AOS.init({
+    duration: 1000,
+    offset: 100,
+});
+
+const trendingSushis = [
+    'Make Sushi',
+    'Nilgiri Sushi',
+    'Oshizushi',
+    'Temaki Sushi',
+    'Uramaki Sushi',
+    'Inari Sushi'
+
+]
